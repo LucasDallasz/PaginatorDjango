de6ac87aecf574457c32af13d1587e26dbd621ce
Dallasz <grave-theboss@hotmail.com>
2021-11-18 20:40:25 -0300

    Métodos alterados

--- paginator.py
+++ paginator.py
@@ -1,80 +1,74 @@
-class Paginator:
+class Page:
+    
+    def __init__(self, elements, page, paginator) -> None:
+        self.elements = elements
+        self.page = page
+        self.paginator = paginator
+        self.length = len(self.elements)
 
+
+    def has_next(self) -> bool:
+        return self.page != len(self.paginator.pages) 
+    
+    
+    def has_previous(self):
+        return self.page > 1
+    
+    
+    def next_page_number(self):
+        return self.page + 1 if self.has_next() else None
+    
+    
+    def previous_page_number(self):
+        return self.page - 1 if self.has_previous() else None
+
+
+    def get_first_value(self):
+        return self.elements[0]
+
+
+    def get_last_value(self):
+        return self.elements[-1]
+    
+    
+    
+class Paginator:
+    
     def __init__(self, objects, per_page) -> None:
         self.objects = objects
         self.per_page = per_page
         self.pages = self._gen_pages()
         self.length = len(self.pages)
-
-
+        
+        
     def _gen_pages(self) -> list:
         result = []
-           
+        
         list_aux, x = [], 1
         
         for obj in self.objects:
-            if x <= self.per_page: 
+            if x <= self.per_page:
                 list_aux.append(obj)
             else:
                 result.append(list_aux)
                 list_aux = []
                 list_aux.append(obj)
                 x = 1
-               
             x += 1
             
         result.append(list_aux)
-                
+    
         return result
-            
-            
- 
+
+
     def get_page(self, page) -> object:
         try:
-            self.pages[page - 1]
+            objectList = self.pages[int(page) - 1]
+        except (TypeError, ValueError):
+            raise ValueError('Inteiro esperado, recebido: ', type(page))
         except IndexError:
-            raise IndexError('Invalid index')
+            raise IndexError('Valor de índice incorreto.')
         else:
-            return Page(self.objects, page, self)
-            
-            
-class Page:
-    
-    
-    def __init__(self, object_list, number_page, paginator) -> None:
-        self.object_list = object_list
-        self.number_page = number_page
-        self._paginator = paginator
-            
-            
-    def has_next(self) -> bool:
-        return self.number_page != len(self._paginator.pages)      
-    
-    
-    def has_previous(self) -> bool:
-        return self.number_page > 1
-    
-    
-    def next_page_number(self) -> int:
-        return self.number_page + 1  if self.has_next() else None
-    
-    
-    def previous_page_number(self) -> int:
-        return self.number_page - 1 if self.has_previous() else None
-          
-          
-    
-def main():
-
-    objects = [{'name': 'Lucas', 'i': i + 1} for i in range(10)]
-    
-    pages = Paginator(objects, 2)
-
-    page = pages.get_page(1)
-    
-    print(page.previous_page_number())
-    
-    
+            return Page(objectList, page, self)
 
 
-main()
